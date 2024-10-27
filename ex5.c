5fa17956614a7fd7e3a270f5e345e1814b1c58cc
LameeAlharkosh <jokerasamt2020@gmail.com>
2024-10-27 19:21:47 +0300

    4

--- ex5.c
+++ ex5.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+#include <cs50.h>
+void sayHello(string name);
+int main(void){
+string name = get_string("Enter your name:");
+sayHello(name);
+}
+void sayHello(string name){
+    printf("Hello, %s\n", name);
+}
